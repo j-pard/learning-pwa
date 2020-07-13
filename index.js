54064a79a25294893bee259fb0fb41ffa69ace36
J-Pard <pardons.jonathan@gmail.com>
2020-07-13 15:45:28 +0200

    Port adjustement

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const path = require('path')
 
-const PORT = 5000;
+const PORT = 3000;
 
 const app = express();
 
@@ -12,6 +12,6 @@ app.get('*', (req, res) => {
       res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT || PORT, () => {
       console.log("Server running on port " + PORT);
 });
